a4185d5daa9f733ada6bcd0e10c9eecb3794a450
kidlocchikapa <bed-com-48-22@unima.ac.mw>
2025-07-30 19:47:33 +0200

    ahh this one is suitable for my ayuu lol

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 void main(){
-  int Age = 21;
+  /**int Age = 21;
   String Cname = "kidloc";
   print("There was a is named ${Cname} with age ${Age}");
 
@@ -8,5 +8,24 @@ void main(){
   print("There was a is named ${Cname} with age ${Age}");
 
   bool isRegisteredVoter= true;
-  print(isRegisteredVoter);
+  print(isRegisteredVoter);**/
+
+  //length is a string method
+  String name = "Kidloc";
+  print("The length of the name is ${name.length}");
+
+  //toUpperCase is a string method
+  String upperName = name.toUpperCase();
+  print("The name in uppercase is ${upperName}");
+
+  //A contains method
+  String sentence = "Kidloc is a great developer";
+  bool containsWord = sentence.contains("great");
+  print("Does the sentence contain 'great'? ${containsWord}");
+
+  //concatenation of two strings
+  String myName = "Kidloc";
+  String mySurname = "Chloe";
+
+  print (mySurname + " " + myName);
 }
