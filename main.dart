51976ab1bee8808fbc38f5d0cc2377ba60a9071e
kidlocchikapa <bed-com-48-22@unima.ac.mw>
2025-07-30 19:28:03 +0200

    First dart learning commit lol

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main(){
+  int Age = 21;
+  String Cname = "kidloc";
+  print("There was a is named ${Cname} with age ${Age}");
+
+
+  Cname = "ChloesDad";
+  print("There was a is named ${Cname} with age ${Age}");
+
+  bool isRegisteredVoter= true;
+  print(isRegisteredVoter);
+}
